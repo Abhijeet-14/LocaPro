3666f299182397ed86e09b75b405b60bce72fbde
Abhijeet-14 <abhijeet.rts@gmail.com>
2019-11-02 04:03:17 +0530

    1st Nov

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, session
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("First_page.html")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/First_page", methods=["POST"])
+def welcome():
+    name=request.form.get("name")
+    password=request.form.get("password")
+    return render_template("First_page.html", name=name , password=password)
 
 if __name__ == "__main__":
     app.run()
